f0c9e10c738a3fc9f6f0bf8f76a0348a78206c2e
Brandon <bcastillo@capmega.com>
2019-12-26 18:51:42 -0600

    Cambio del splashScreen y la statusBar por capacitor/core en lugar de native

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,6 @@
-import { Component } from '@angular/core';
-
-import { Platform } from '@ionic/angular';
-import { SplashScreen } from '@ionic-native/splash-screen/ngx';
-import { StatusBar } from '@ionic-native/status-bar/ngx';
+import {Component} from '@angular/core';
+import {Platform} from '@ionic/angular';
+import {Plugins, StatusBarStyle} from '@capacitor/core'
 
 @Component({
   selector: 'app-root',
@@ -25,17 +23,22 @@ export class AppComponent {
   ];
   constructor(
     private platform: Platform,
-    private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+
   ) {
     this.initializeApp();
   }
 
-  initializeApp() {
-    this.platform.ready().then(() => {
-      this.statusBar.styleDefault();
-      this.splashScreen.hide();
-    });
+  async initializeApp() {
+    const { SplashScreen, StatusBar } = Plugins;
+    try {
+      await SplashScreen.hide();
+      await StatusBar.setStyle({ style: StatusBarStyle.Light});
+      if(this.platform.is('android')) {
+        await StatusBar.setBackgroundColor({ color: '#CDCDCD'})
+      }
+    } catch (error) {
+        console.log('This is normal in a browser', error);
+    }
   }
 
 
